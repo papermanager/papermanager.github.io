e296fd566cf8a748f7ad2b287643bb1134f9f355
christosle <christosle@users.noreply.github.com>
2022-04-16 00:31:31 +0000

    deploy: bad122dd1d775c3c7e04d8f38e5d1eba1c09ecf5

---
+++ assets/js/489341c1.fbb01333.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpapermanager_docs=self.webpackChunkpapermanager_docs||[]).push([[299],{3769:function(e){e.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-docs","id":"default"}}')}}]);
---
+++ assets/js/6c1f2d4b.ecafcbda.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpapermanager_docs=self.webpackChunkpapermanager_docs||[]).push([[765],{5745:function(e){e.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-pages","id":"default"}}')}}]);
